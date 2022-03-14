d1c720305f96e2bdf3d03546d41a9d4f90cc3fdd
logica <84759675+logica0419@users.noreply.github.com>
2022-03-14 06:58:25 +0000

    added test for `/user/update`

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,9 +5,10 @@ from app.api import app
 client = TestClient(app)
 
 
-def test_create_user():
+def test_user():
   response = client.post(
-      "/user/create", json={"user_name": "test1", "leader_card_id": 1000}
+      "/user/create",
+      json={"user_name": "test1", "leader_card_id": 1000}
   )
   assert response.status_code == 200
 
@@ -18,11 +19,26 @@ def test_create_user():
 
   response = client.get(
       "/user/me",
-      headers={
-          "Authorization": f"bearer {token}"})
+      headers={"Authorization": f"bearer {token}"})
   assert response.status_code == 200
 
   response_data = response.json()
   assert response_data.keys() == {"id", "name", "leader_card_id"}
   assert response_data["name"] == "test1"
   assert response_data["leader_card_id"] == 1000
+
+  response = client.post(
+      "/user/update",
+      headers={"Authorization": f"bearer {token}"},
+      json={"user_name": "test2", "leader_card_id": 2000})
+  assert response.status_code == 200
+
+  response = client.get(
+      "/user/me",
+      headers={"Authorization": f"bearer {token}"})
+  assert response.status_code == 200
+
+  response_data = response.json()
+  assert response_data.keys() == {"id", "name", "leader_card_id"}
+  assert response_data["name"] == "test2"
+  assert response_data["leader_card_id"] == 2000
